fbcbb9072d673b1e2bc95ee7315e1079bef69af4
BDanl <alejandrobovea@gmail.com>
2023-10-22 15:37:14 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction(val) {
+    if (val=== 1) {
+        document.getElementById('parq').style.display='none';
+    } 
+    if (val==2){
+        document.getElementById('parq').style.display='block';
+    }
+}
+
